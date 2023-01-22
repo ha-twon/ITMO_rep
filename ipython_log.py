e08dab8b4de07315eb3cc907a62c533108566d8e
ha-twon <123307348+ha-twon@users.noreply.github.com>
2023-01-22 19:29:05 +0300

    Add files via upload

---
+++ ipython_log.py
@@ -0,0 +1,26 @@
+# IPython log file
+
+bin(1234567)
+#[Out]# '0b100101101011010000111'
+0b00110011 | 128
+#[Out]# 179
+bin(0b00110011 | 128)
+#[Out]# '0b10110011'
+a = 0b00110011
+-10 < a < 100
+#[Out]# True
+42 << 34
+#[Out]# 721554505728
+int(13.8)
+#[Out]# 13
+13.888.__ceil__()
+print("AttributeError: 'float' object has no attribute __ceil__")
+import math
+a = 13.888
+math.ceil(a)
+#[Out]# 14
+math.floor(a)
+#[Out]# 13
+round(a,2)
+#[Out]# 13.89
+exit()
